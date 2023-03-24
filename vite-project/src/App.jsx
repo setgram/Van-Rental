227f7cfdb10bd434816138331603508730b38e12
Wisdom Okogho <91355531+setgram@users.noreply.github.com>
2023-03-24 11:57:14 +0100

    Adding files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
